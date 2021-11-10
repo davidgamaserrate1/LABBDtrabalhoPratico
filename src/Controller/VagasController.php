55921cb8309781d0f8cbd974559032cee1c0b74f
davidgamaserrate <tetigamaserrate@gmail.com>
2021-11-10 18:41:27 -0300

    projeto finalizado!

---
+++ src/Controller/VagasController.php
@@ -0,0 +1,116 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Vagas Controller
+ *
+ * @property \App\Model\Table\VagasTable $Vagas
+ * @method \App\Model\Entity\Vaga[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class VagasController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Departamentos', 'Empresas', 'EstabelecimentosEmpresas', 'Setores'],
+        ];
+        $vagas = $this->paginate($this->Vagas);
+
+        $this->set(compact('vagas'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Vaga id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $vaga = $this->Vagas->get($id, [
+            'contain' => ['Departamentos', 'Empresas', 'EstabelecimentosEmpresas', 'Setores'],
+        ]);
+
+        $this->set(compact('vaga'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $vaga = $this->Vagas->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $vaga = $this->Vagas->patchEntity($vaga, $this->request->getData());
+            if ($this->Vagas->save($vaga)) {
+                $this->Flash->success(__('Vaga salva com sucesso!'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The vaga could not be saved. Please, try again.'));
+        }
+        $departamentos = $this->Vagas->Departamentos->find('list', ['limit' => 200])->all();
+        $empresas = $this->Vagas->Empresas->find('list', ['limit' => 200])->all();
+        $estabelecimentosEmpresas = $this->Vagas->EstabelecimentosEmpresas->find('list', ['limit' => 200])->all();
+        $setores = $this->Vagas->Setores->find('list', ['limit' => 200])->all();
+        $this->set(compact('vaga', 'departamentos', 'empresas', 'estabelecimentosEmpresas', 'setores'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Vaga id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $vaga = $this->Vagas->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $vaga = $this->Vagas->patchEntity($vaga, $this->request->getData());
+            if ($this->Vagas->save($vaga)) {
+                $this->Flash->success(__('The vaga has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The vaga could not be saved. Please, try again.'));
+        }
+        $departamentos = $this->Vagas->Departamentos->find('list', ['limit' => 200])->all();
+        $empresas = $this->Vagas->Empresas->find('list', ['limit' => 200])->all();
+        $estabelecimentosEmpresas = $this->Vagas->EstabelecimentosEmpresas->find('list', ['limit' => 200])->all();
+        $setores = $this->Vagas->Setores->find('list', ['limit' => 200])->all();
+        $this->set(compact('vaga', 'departamentos', 'empresas', 'estabelecimentosEmpresas', 'setores'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Vaga id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $vaga = $this->Vagas->get($id);
+        if ($this->Vagas->delete($vaga)) {
+            $this->Flash->success(__('The vaga has been deleted.'));
+        } else {
+            $this->Flash->error(__('The vaga could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ templates/Vagas/index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Vaga[]|\Cake\Collection\CollectionInterface $vagas
+ */
+?>
+<div class="vagas index content">
+    <?= $this->Html->link(__('New Vaga'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+    <h3><?= __('Vagas') ?></h3>
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('departamento_id') ?></th>
+                    <th><?= $this->Paginator->sort('empresa_id') ?></th>
+                    <th><?= $this->Paginator->sort('estabelecimentos_empresa_id') ?></th>
+                    <th><?= $this->Paginator->sort('setore_id') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($vagas as $vaga): ?>
+                <tr>
+                    <td><?= $this->Number->format($vaga->id) ?></td>
+                    <td><?= $vaga->has('departamento') ? $this->Html->link($vaga->departamento->nome_departamento, ['controller' => 'Departamentos', 'action' => 'view', $vaga->departamento->id]) : '' ?></td>
+                    <td><?= $vaga->has('empresa') ? $this->Html->link($vaga->empresa->nome_empresa, ['controller' => 'Empresas', 'action' => 'view', $vaga->empresa->id]) : '' ?></td>
+                    <td><?= $vaga->has('estabelecimentos_empresa') ? $this->Html->link($vaga->estabelecimentos_empresa->empresa_nome, ['controller' => 'EstabelecimentosEmpresas', 'action' => 'view', $vaga->estabelecimentos_empresa->id]) : '' ?></td>
+                    <td><?= $vaga->has('setore') ? $this->Html->link($vaga->setore->nome_setor, ['controller' => 'Setores', 'action' => 'view', $vaga->setore->id]) : '' ?></td>
+                    <td class="actions">
+                        <?= $this->Html->link(__('View'), ['action' => 'view', $vaga->id]) ?>
+                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $vaga->id]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $vaga->id], ['confirm' => __('Are you sure you want to delete # {0}?', $vaga->id)]) ?>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="paginator">
+        <ul class="pagination">
+            <?= $this->Paginator->first('<< ' . __('primeiro')) ?>
+            <?= $this->Paginator->prev('< ' . __('anterior')) ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next(__('proximo') . ' >') ?>
+            <?= $this->Paginator->last(__('último') . ' >>') ?>
+        </ul>
+        <p><?= $this->Paginator->counter(__('Página {{page}} de {{pages}}, Exibindo {{current}} registros(s) de {{count}}')) ?></p>
+    </div>
+</div>
